56be442151f4f0f7f7886d3a4d52763814bce706
Reem Ali <reemalielesh@gmail.com>
2022-07-03 17:21:00 +0200

    change API url

--- lib/shared/dio_helper.dart
+++ lib/shared/dio_helper.dart
@@ -6,7 +6,8 @@ class DioHelper {
   static init() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://emanmo1-001-site1.btempurl.com/api/',
+        //baseUrl: 'http://emanmo1-001-site1.btempurl.com/api/',
+        baseUrl: 'http://saasproject-001-site1.itempurl.com/api/',
         receiveDataWhenStatusError: true,
       ),
     );
